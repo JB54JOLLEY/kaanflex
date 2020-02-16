1ef3d814000fe3f4c8411d53e5ae801f0a5c1aa3
Glitch (sideways-order) <none>
2020-02-16 19:22:23 +0000

    🎖💑 Checkpoint ./komutlar/davet.js:3324753/1535 ./komutlar/botbilgi.js:3324753/2207

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -1,20 +1,21 @@
-const Discord = require('discord.js'); 
- 
-exports.run = async(client, message, args) => {
- 
- const Embed = new Discord.RichEmbed()
- .setTimestamp()
- .setAuthor("ST-AT", client.user.avatarURL)
-.setColor("BLUE")
-.setTitle("ST-AT BOT")
- .setURL("https://discordapp.com/oauth2/authorize?client_id=647386467844227074&scope=bot&permissions=8")
-  .setDescription(`Sunucuna İstatistik Paneli Yapmak İçin Davet Edebilirsin`)
+const Discord = require("discord.js");
 
- .addField("Davet Linki.", `https://www.google.com/`)
+exports.run = async (client, message, args) => {
+  const Embed = new Discord.RichEmbed()
+    .setTimestamp()
+    .setAuthor("ST-AT", client.user.avatarURL)
+    .setColor("BLUE")
+    .setTitle("ST-AT BOT")
+    .setURL(
+      "https://discordapp.com/oauth2/authorize?client_id=647386467844227074&scope=bot&permissions=8"
+    )
+    .setDescription(`Sunucuna İstatistik Paneli Yapmak İçin Davet Edebilirsin`)
 
- .setFooter("© Mcadventuretime.com", client.user.avatarURL)
- message.channel.send(Embed)
-}
+    .addField("Davet Linki.", `https://www.google.com/`)
+
+    .setFooter("© Mcadventuretime.com", client.user.avatarURL);
+  message.channel.send(Embed);
+};
 
 module.exports.conf = {
   enabled: true,
@@ -24,7 +25,7 @@ module.exports.conf = {
 };
 
 module.exports.help = {
-  name: 'davet',
-  description: 'Davet Et.',
-  usage: 'davet'
+  name: "davet",
+  description: "Davet Et.",
+  usage: "davet"
 };
